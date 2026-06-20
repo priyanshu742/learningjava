8e40f8691c97aeb95f8b7800e889a12f1442688c
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-06-21 00:05:00 +0530

    understanding primitive datatypes

---
+++ data.java
@@ -0,0 +1,28 @@
+class data
+{
+    public static void main(String a[])
+        {
+            int num=3;
+            byte num1=127;  //testing range actually
+            long l=5678l;
+            short sh=888;
+     
+            float f=98.8f;
+            double d=987.654321;
+
+            char ch='k';
+     
+            boolean b=true;
+            System.out.println(num);
+            System.out.println(num1);
+            System.out.println(l);
+            System.out.println(sh);
+            System.out.println(f);
+            System.out.println(d);
+            System.out.println(ch);
+            System.out.println(b);
+        }
+}        
+
+
+
