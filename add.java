a0ce34dfa2941583902dc693d29f4cd3e4db5e9c
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-06-21 00:01:53 +0530

    Learning how to add numbers directly and by use of variables

---
+++ add.java
@@ -0,0 +1,14 @@
+class add
+{
+    public static void main(String a[])
+    {
+        int num=3;
+        int num1=5;
+        int result=num+num1;
+        System.out.println(result);
+        System.out.println(9+5);
+        System.out.println(8+7);  
+        System.out.println(num);
+        System.out.println(num1+num);
+    }
+}
