9bfa0c034147b4a1620e57facbfac83f10760206
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-06-21 22:18:00 +0530

    learning relational operators

---
+++ relationaloperators.java
@@ -0,0 +1,47 @@
+class relational
+{
+    public static void main(String a[])
+    {
+        int x=10,b=20;
+        boolean result=x<b;
+        System.out.println(result);
+
+
+
+        boolean result1=x>b;
+        System.out.println(result1);
+
+
+        int c=30,d=30;
+        boolean result2=c>d;
+        System.out.println(result2);
+
+
+        boolean result3=c>=d;
+        System.out.println(result3);
+
+
+        boolean result4=c<=d;
+        System.out.println(result4);
+
+
+        int e=40,f=50;
+        boolean result5=e != f;
+        System.out.println(result5);
+        boolean result6=e == f;
+        System.out.println(result6);
+
+
+        boolean result7=c == d;
+        System.out.println(result7);
+
+        double g=10.5;
+        float h=77.9f;
+        boolean result8=g >= h;
+        System.out.println(result8);
+        boolean result9=g <= h;
+        System.out.println(result9);
+
+        
+    }
+}
