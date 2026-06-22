270142fbd6c1c8eaf04656f8058d0b817ce8ef5e
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-06-22 16:44:29 +0530

    Experimenting ternary operator

---
+++ ternaryoperator.java
@@ -0,0 +1,29 @@
+class ternary
+{
+    public static void main(String[]args)
+    {
+        int n=9;
+        int result=0;
+
+        /*
+        
+        
+            if(n%2==0)
+            result=4; if number is even then result will be 4
+        else
+            result=5; if number is odd then result will be 5
+
+            System.out.println(result);
+
+
+        */
+
+
+        // using ternary operator to write above code in single line 
+
+        result= n%2==0 ?  4:  5;    //  condition? value if true : value if false 
+        System.out.println(result);
+
+    }
+    
+}
