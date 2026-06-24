04a4641b31987566e1d0b3d6f7126746e9336337
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-06-24 23:49:20 +0530

    understanding all types of loops with examples

--- loops.java
+++ loops.java
@@ -1,9 +1,19 @@
+
+class student  // for array of objects
+        {
+            int rollno;
+            String name;
+            int marks;
+        }
+
+
 public class loops
 {
     public static void main(String args[])
     {
-        // using while loop
-  
+
+     // using while loop
+
 
         int i=1;
         while(i<=4)
@@ -13,7 +23,7 @@ public class loops
         }
         System.out.println("bye" + i);
 
-
+        System.out.println(); 
       
        //using nested while loop
 
@@ -30,6 +40,7 @@ public class loops
             j++;
         }
 
+        System.out.println(); 
 
         // using do while loop
         // the loop is executed atleast once even if the condition is false
@@ -42,10 +53,9 @@ public class loops
             l++;
         }while(i<=4);
 
+        System.out.println(); 
 
         // using do while loop again
-
-
 
         int m=1;
         do    
@@ -54,6 +64,7 @@ public class loops
             m++;
         }while(m<=4);
 
+        System.out.println(); 
 
         // using for loop
 
@@ -62,21 +73,29 @@ public class loops
             System.out.println("key" + n);
         }
 
+        System.out.println(); 
+
         for(int p=1;p<=4;p+=2)
         {
             System.out.println("key" + p);
         }
+
+        System.out.println(); 
 
         for(int r=4;r>=1;r--)
         {
             System.out.println("key" + r);
         }
 
+        System.out.println(); 
+
         int s=0;
         for(s=0;s<=3;s++)       //for(int s=0;s<4;s++)
         {
             System.out.println("bread" + s);
         }
+
+        System.out.println(); 
 
 
         //using nested for loop
@@ -91,7 +110,52 @@ public class loops
             }
         } 
 
+        System.out.println(); 
 
+        // using enhanced for loop or for-each loop
+
+        int num1[]=new int[5];
+        System.out.println(num1[2]);  // {0,0,0,0,0}
+        num1[0]=3;
+        num1[1]=6;
+        num1[2]=9;
+        num1[3]=12;
+        System.out.println(num1[2]); // {3,6,9,12,0}
+
+        System.out.println(); 
+
+        for(int n: num1)
+        {
+            System.out.println(n); // printing array using enhanced for loop
+        }
+
+        System.out.println();
+
+        student s1=new student();
+        s1.rollno=22;
+        s1.name="jonas";
+        s1.marks=87;
+
+        student s2=new student();
+        s2.rollno=23;
+        s2.name="martha";
+        s2.marks=22;
+
+        student s3=new student();
+        s3.rollno=22;
+        s3.name="mikkel";
+        s3.marks=90;
+
+        student stu[]=new student[3];
+        stu[0]=s1;
+        stu[1]=s2;
+        stu[2]=s3;
+
+        System.out.println("name" + " : " + "roll no" + " : " + "marks");
+        for(student stud: stu)
+        {
+            System.out.println(stud.name + " : " + stud.rollno + " : " + stud.marks); // printing array of objects using enhanced for loop
+        }
     }
 
 }
@@ -103,6 +167,7 @@ Hi2
 Hi3
 Hi4
 bye5
+
 Hi1
 Hello1
 Hello2
@@ -119,25 +184,32 @@ Hi4
 Hello1
 Hello2
 Hello3
+
 new9
+
 old1
 old2
 old3
 old4
+
 key1
 key2
 key3
 key4
+
 key1
 key3
+
 key4
 key3
 key2
 key1
+
 bread0
 bread1
 bread2
 bread3
+
 DAY1
     9-10
     10-11
@@ -209,4 +281,21 @@ DAY7
     16-17
     17-18
 
-    */
+0
+9
+
+3
+6
+9
+12
+0
+
+name : roll no : marks
+jonas : 22 : 87
+martha : 23 : 22
+mikkel : 22 : 90
+
+[Done] exited with code=0 in 0.255 seconds
+
+*/
+
