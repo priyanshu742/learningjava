9f82266221f830820091cd54f11b627a46a0ffcb
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-06-26 20:23:33 +0530

    Naming convention rules

---
+++ aanamingconvention.java
@@ -0,0 +1,24 @@
+public class aanamingconvention
+ {
+    public static void main(String args[])
+    {
+        System.out.println("RULES ARE");
+        
+        // class name= Human (first letter capital)
+
+        // variables=marks (all letter small)
+
+        // methods=show()  (all letter small)
+
+        // constants=PIE,BRAND (all letter capital)
+
+        // camelCasing ie MyData,showMyMarks()
+
+        // some examples are
+        // age (variable)
+        // DATA (constant). MY_DATA (constant)
+        // Human() (constructor)
+    }
+   
+    
+ }
