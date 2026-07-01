e6d157537f226f95cbf3f0b797b75c81edb8f2f8
priyanshusingh <priyanshusingh16122007@gmail.com>
2026-07-02 00:52:05 +0530

    Understanding the runnable interface and how to use it with thread

---
+++ runnableThread.java
@@ -0,0 +1,47 @@
+
+
+public class runnableThread 
+{
+    public static void main(String[] args) 
+    {
+        Runnable m3= () -> // runnable interface is a functional interface so we can use lambda expression to implement it
+            {
+                for(int i=1;i<=5;i++)
+                {
+                    System.out.println(" hi " + i);
+                    try 
+                    {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e) 
+                    {
+                         e.printStackTrace();
+                    } 
+                }
+            };
+    
+        Runnable m4= () -> 
+            {
+                for(int i=1;i<=5;i++)
+                {
+                    System.out.println(" hello " +i);
+                    try
+                    {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e) 
+                    {
+                         e.printStackTrace();
+                    }
+
+                }
+            
+            };
+
+        Thread t1=new Thread(m3);
+        Thread t2=new Thread(m4);
+
+        t1.start();
+        t2.start();
+    }     
+}
